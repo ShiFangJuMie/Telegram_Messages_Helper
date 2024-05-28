fa876908035275c71034fb355f0f962ad6c3c610
ShiFangJuMie <shifangjumie@gmail.com>
2024-05-28 14:59:56 +0800

    改进提示词

--- app/script_aigc.py
+++ app/script_aigc.py
@@ -20,8 +20,10 @@ def load_prompt_from_file(filepath):
 
 
 def call_ai_api(message):
+    if len(message) <= 300:
+        return "300个字都不到的群，没有总结价值。"
     if AI_API_MODEL == 'coze' and len(PROMPT)+len(message) >= 60000:
-        return "输入的文本长度超过60000字符，无法总结。"
+        return "超过60000字符，超出CDP可用的最大上下文，无法总结。"
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {AI_API_KEY}'
