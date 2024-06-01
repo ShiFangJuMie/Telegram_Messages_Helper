871ca4969ab2cb5070806e7e30e6d376fe535621
ShiFangJuMie <shifangjumie@gmail.com>
2024-06-01 15:55:15 +0800

    改进提示词

--- app/script_aigc.py
+++ app/script_aigc.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timedelta
 
 import psycopg2
 import psycopg2.extras
@@ -14,6 +15,7 @@ db = Database(db_params)
 AI_API_URL = os.getenv('AI_API_URL')
 AI_API_KEY = os.getenv('AI_API_KEY')
 AI_API_MODEL = os.getenv('AI_API_MODEL')
+max_tokens = 4096
 
 
 def load_prompt_from_file(filepath):
@@ -22,11 +24,12 @@ def load_prompt_from_file(filepath):
 
 
 def call_ai_api(message):
-    global AI_API_MODEL
-    if len(message) <= 100:
-        return "100个字都不到的群，没有总结价值。"
+    global AI_API_MODEL, max_tokens
+    if len(message) <= 200:
+        return f"本次总结因内容太少而跳过，以下为原文：\n{message}"  # 如果消息长度小于200，直接返回原文
     if AI_API_MODEL == 'coze' and len(PROMPT) + len(message) >= 128000:
         AI_API_MODEL = 'gemini'
+        max_tokens = 8192
         # return "上下文超过128k，超出CDP(GPT-4o)极限，无法总结。"
     if AI_API_MODEL == 'gemini' and len(PROMPT) + len(message) >= 200000:
         return "上下文超过200k，超出CDP(Gemini)极限，无法总结。"
@@ -42,7 +45,7 @@ def call_ai_api(message):
             }
         ],
         'model': AI_API_MODEL,
-        'max_tokens': 8192,
+        'max_tokens': max_tokens,
         'temperature': 0.8,
         'stream': False
     }
@@ -50,8 +53,9 @@ def call_ai_api(message):
         response = requests.post(AI_API_URL, headers=headers, json=data)
         response.raise_for_status()  # 如果响应状态码不是200，抛出HTTPError
         result = response.json()
+        response_content = result['choices'][0]['message']['content'].strip()
         # 返回需要写入数据库的文本
-        return result['choices'][0]['message']['content'].strip()
+        return f"本次总结由{AI_API_MODEL}模型驱动：\n{response_content}"
     except requests.exceptions.RequestException as e:
         logging.error(f"API request failed: {e}")
         return None
@@ -62,12 +66,17 @@ def process_aggregated_messages():
     cur = conn.cursor()
 
     try:
+        # 获取当前日期和1天前的日期
+        today = datetime.now().date()
+        yesterday = today - timedelta(days=1)
+
         # 查询聚合消息
         cur.execute("""
             SELECT id, messages
             FROM messages_aggregated
             WHERE ai_summary IS NULL
-        """)
+            AND aggregated_date >= %s
+        """, (yesterday,))
 
         rows = cur.fetchall()
         for row_id, messages in rows:
@@ -96,7 +105,7 @@ def process_aggregated_messages():
 
 if __name__ == '__main__':
     # 从文件中加载预设的prompt
-    PROMPT_FILE_PATH = 'prompt.txt'  # 假设文件名为 prompt.txt
+    PROMPT_FILE_PATH = 'prompt.txt'
     try:
         PROMPT = load_prompt_from_file(PROMPT_FILE_PATH)
     except Exception as e:
